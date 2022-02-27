b04ab5b0071f515320c8b315f665ff3caf9a1788
CHRIS_LEE <chrismlee87@gmail.com>
2022-02-26 21:59:16 -0800

    [update] link type with id, url, description and extend type

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -9,3 +9,28 @@ export const Link = objectType({
         t.nonNull.string("url"); // Add a 'url field', type String
     },
 });
+
+let links: NexusGenObjects["Link"][]= [ // Use "links" variable to store links at runtime (in memory)
+  {
+      id: 1,
+      url: "www.howtographql.com",
+      description: "Fullstack tutorial for GraphQL",
+  },
+  {
+      id: 2,
+      url: "graphql.org",
+      description: "GraphQL official website",
+  },
+];
+
+export const LinkQuery = extendType({ // Extend "Query" root type, adding a new root field: "feed"
+  type: "Query",
+  definition(t) {
+      t.nonNull.list.nonNull.field("feed", { // Define "feed" return type query as not nullable array of type "Link"
+          type: "Link",
+          resolve(parent, args, context, info) { // Resolver function of "feed" query: "resolve". arcs = "parent", "args", "context", "info"
+              return links;
+          },
+      });
+  },
+});
